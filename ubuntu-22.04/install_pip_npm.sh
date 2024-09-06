da34076d9b6acee14c1e12d7f3649a284bf8a9a4
白いねこ <115929530+shiroinekotfs@users.noreply.github.com>
2024-09-06 20:55:19 +0700

    Update

--- ubuntu-22.04/install_pip_npm.sh
+++ ubuntu-22.04/install_pip_npm.sh
@@ -12,21 +12,14 @@ for ins in pip setuptools wheel "manimlib==0.2.0" pycairo; do
     fi
 done
 
+pip install -r ./.global/pip_base.txt --default-timeout=360 --use-pep517
+
 echo -e "Building IBM-Q Packages"
-IBM_Q_BUILD_SQ="python3 -m build --wheel --outdir ./.global/ext-pkg --skip-dependency-check --no-isolation ./.global/ext-pkg/ibm-q-labs"
 for ibmqpkg in ibm_q_lab_server_extension ibm_q_lab_ui_extensions ibm_quantum_widgets ibmq_jupyter_server_health_ext qiskit-kernel; do
-    $IBM_Q_BUILD_SQ/$ibmqpkg
-    if [ $? -ne 0 ]; then 
-        echo -e -n "Error while installing $ibmqpkg. Retrying..."
-        $IBM_Q_BUILD_SQ/$ibmqpkg
-    fi
+    python3 -m build --wheel --outdir ./.global/ext-pkg --skip-dependency-check --no-isolation ./.global/ext-pkg/ibm-q-labs/$ibmqpkg
 done
 
-PIP_INS="pip install -r ./.global/pip_base.txt --default-timeout=360 --use-pep517 && pip install ./.global/ext-pkg/*.whl --ignore-installed --no-deps --use-pep517"
-$PIP_INS; if [ $? -ne 0 ]; then
-    echo -e -n "Error while installing, retrying..."
-    $PIP_INS
-fi
+pip install ./.global/ext-pkg/*.whl --ignore-installed --no-deps --use-pep517
 
 echo -e "Disabling the classic mode"
 jupyter lab build
